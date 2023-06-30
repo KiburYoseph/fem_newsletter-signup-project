f5bf0e43eb583ce032583212a2e94a0e22ae9348
KiburYoseph <kiburstgr@gmail.com>
2023-06-30 10:04:02 +0300

    Thank You Section Done

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ const submitButton = document.querySelector('.submit');
 const emailInput = document.querySelector('#email');
 const wrongInput = document.querySelector('.wrongInput');
 const form = document.querySelector('.emailInput');
+const initialForm = document.querySelector('.initialForm');
+const thankYouPage = document.querySelector('.thankYouPage');
+let userEmail;
 
 function ValidateEmail(inputText)
     {
         var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
         if(inputText.value.match(mailformat))
             {
-                form.submit();
+                userEmail = emailInput.value;
+                initialForm.style.opacity = "0";
                 return true;
             }
         else
